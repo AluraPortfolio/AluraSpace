c2b9fff39e4822a68019bafcf5accc0e6e7c324b
Vinicius <viniciusfelicianomello@hotmail.com>
2024-06-20 18:37:13 -0300

    Alterações da primeira aula do curso

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.model):
+    # null=false é required e blank=false não pode ser vazia ''
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto= models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
+        
